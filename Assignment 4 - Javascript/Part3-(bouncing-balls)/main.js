854c3402896ac062e18fccadaa6d28a63466ee37
ASHISH AJAY <163020665+AASHIX@users.noreply.github.com>
2026-07-31 01:27:32 -0400

    Change Log: All the corrections has been completed.

--- Assignment 4 - Javascript/Part3-(bouncing-balls)/main.js
+++ Assignment 4 - Javascript/Part3-(bouncing-balls)/main.js
@@ -1,4 +1,4 @@
-// set up canvas
+// setup canvas
 
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
@@ -12,11 +12,13 @@ function random(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// function to generate random RGB color value
+// function to generate random color
 
 function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
+
+//Modeling a ball in our program
 
 class Ball {
   constructor(x, y, velX, velY, color, size) {
@@ -27,38 +29,35 @@ class Ball {
     this.color = color;
     this.size = size;
   }
-
-  draw() {
+  draw() { //Drawing the ball
     ctx.beginPath();
     ctx.fillStyle = this.color;
     ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
     ctx.fill();
   }
-
-  update() {
+  update() { //Updating the ball's data
     if (this.x + this.size >= width) {
-      this.velX = -Math.abs(this.velX);
+      this.velX = -this.velX;
     }
 
     if (this.x - this.size <= 0) {
-      this.velX = Math.abs(this.velX);
+      this.velX = -this.velX;
     }
 
     if (this.y + this.size >= height) {
-      this.velY = -Math.abs(this.velY);
+      this.velY = -this.velY;
     }
 
     if (this.y - this.size <= 0) {
-      this.velY = Math.abs(this.velY);
+      this.velY = -this.velY;
     }
 
     this.x += this.velX;
     this.y += this.velY;
   }
-
-  collisionDetect() {
+  collisionDetect() { //Adding collision detection
     for (const ball of balls) {
-      if (!(this === ball)) {
+      if (this !== ball) {
         const dx = this.x - ball.x;
         const dy = this.y - ball.y;
         const distance = Math.sqrt(dx * dx + dy * dy);
@@ -70,6 +69,8 @@ class Ball {
     }
   }
 }
+
+//Animating the ball
 
 const balls = [];
 
@@ -83,14 +84,16 @@ while (balls.length < 25) {
     random(-7, 7),
     random(-7, 7),
     randomRGB(),
-    size
+    size,
   );
 
   balls.push(ball);
 }
 
+//Loop
+
 function loop() {
-  ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
+  ctx.fillStyle = "rgb(0 0 0 / 25%)";
   ctx.fillRect(0, 0, width, height);
 
   for (const ball of balls) {
@@ -100,6 +103,4 @@ function loop() {
   }
 
   requestAnimationFrame(loop);
-}
-
-loop();
+}loop();
